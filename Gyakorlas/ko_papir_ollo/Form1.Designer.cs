5e1cb3db6b69015faba6fa6400818b6863f75e5d
Gábor <baligabor@gmail.com>
2025-06-06 13:15:19 +0200

    First

---
+++ Gyakorlas/ko_papir_ollo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ko_papir_ollo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Játék";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(338, 397);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kilépés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kő",
+            "Papír",
+            "Olló"});
+            this.comboBox1.Location = new System.Drawing.Point(292, 180);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Gép:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Játékos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(202, 281);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "0 : 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(468, 30);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kő - papír - olló játék";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ Gyakorlas/ko_papir_ollo/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ko_papir_ollo
+{
+    public partial class Form1 : Form
+    {
+        int gep_pont = 0;
+        int jatekos_pont = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int jatek()
+        {
+            Random rnd = new Random();
+            int gep = rnd.Next(1, 4);
+            int jatekos = 0;
+            int eredmeny = -1;
+
+            if (gep == 1)
+            {
+                textBox1.Text = "Kő";
+            }
+            else if (gep == 2)
+            {
+                textBox1.Text = "Papír";
+            }
+            else
+            {
+                textBox1.Text = "Olló";
+            }
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    jatekos = 1;
+                    break;
+                case 1:
+                    jatekos = 2;
+                    break;
+                case 2:
+                    jatekos = 3;
+                    break;
+            }
+
+            if (jatekos == gep)
+            {
+                eredmeny = 2;
+            }
+            else if (jatekos == 1 && gep == 3 || jatekos == 2 && gep == 1 || jatekos == 3 && gep == 2)
+            {
+                eredmeny = 0;
+            }
+            else
+            {
+                eredmeny = 1;
+            }
+
+            return eredmeny;
+        }
+        public void szamolas(int szam)
+        {
+            switch (szam)
+            {
+                case 0:
+                    jatekos_pont += 1;
+                    break;
+                case 1:
+                    gep_pont += 1;
+                    break;
+                case 2:
+                    jatekos_pont += 1;
+                    gep_pont += 1;
+                    break;
+            }
+
+            label4.Text = $"{gep_pont} : {jatekos_pont}";
+        }
+
+        public void nyer()
+        {
+            if(gep_pont == 5 && jatekos_pont < 5)
+            {
+                DialogResult valasz = MessageBox.Show("A gép nyert!", "Vesztettél!", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                if (valasz == DialogResult.OK)
+                {
+                    gep_pont = 0;
+                    jatekos_pont = 0;
+                    label4.Text = $"{gep_pont} : {jatekos_pont}";
+                }
+            }
+            else if (jatekos_pont == 5 && gep_pont < 5)
+            {
+                DialogResult valasz = MessageBox.Show("Ügyes vagy nyertél!", "Nyertél!", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                if (valasz == DialogResult.OK)
+                {
+                    gep_pont = 0;
+                    jatekos_pont = 0;
+                    label4.Text = $"{gep_pont} : {jatekos_pont}";
+                }
+            }
+            else if (gep_pont == 5 && jatekos_pont == 5)
+            {
+                DialogResult valasz = MessageBox.Show("Döntetlen!", "Döntetlen", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                if (valasz == DialogResult.OK)
+                {
+                    gep_pont = 0;
+                    jatekos_pont = 0;
+                    label4.Text = $"{gep_pont} : {jatekos_pont}";
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int adat = jatek();
+            szamolas(adat);
+            nyer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult valasz = MessageBox.Show("Biztosan ki szeretnél lépni?", "Kilépés", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (valasz == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
