a845c96d28940e2d392d4b569b90d352c53cb1cb
Gábor <baligabor@gmail.com>
2025-06-06 13:14:32 +0200

    First

---
+++ Gyakorlas/oophitel/Hitel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opphitel
+{
+    internal class Hitel
+    {
+        public double kamat;
+        public double felvettosszeg;
+        public int evekszama;
+        public double kezelesikoltseg;
+
+        public Hitel(double k, double f, int e)
+        {
+            kamat = k;
+            felvettosszeg = f;
+            kezelesikoltseg = 0.05;
+            evekszama = e;
+        }
+
+        public double visszafizetendo()
+        {
+            double osszeg;
+            osszeg = Math.Round(felvettosszeg + (felvettosszeg * kamat / 100) + (felvettosszeg * kezelesikoltseg));
+
+            return osszeg;
+        }
+
+        public double havitorleszto()
+        {
+            double havi;
+      
+            havi = Math.Round(visszafizetendo() / (evekszama * 12));
+
+            return havi;
+
+        }
+    }
+}
---
+++ Gyakorlas/oophitel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opphitel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Adja meg a felvett összeget: ");
+            double osszeg = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a kamatot: ");
+            double kamat = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg az futamidőt években: ");
+            int ev = Convert.ToInt32(Console.ReadLine());
+
+            Hitel a = new Hitel(kamat, osszeg, ev);
+
+            Console.WriteLine($"A visszafizetendő összeg: {a.visszafizetendo()} Ft");
+            Console.WriteLine($"A havi törlesztőrészlet: {a.havitorleszto()} Ft");
+        }
+    }
+}
