68571bfacc70e702aed805971d6f0a055bcccb23
Gábor <baligabor@gmail.com>
2025-06-12 12:02:54 +0200

    First

---
+++ Gyakorlas/cicukaoop/Macska.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cicukaoop
+{
+    internal class Macska
+    {
+        private string nev;
+        private double suly;
+        private bool ehese;
+
+        public string Nev { get => nev; set => nev = value; }
+        public double Suly { get => suly; set => suly = value; }
+        public bool Ehese { get => ehese; set => ehese = value; }
+
+        public Macska(string n, double s, bool e)
+        {
+            nev = n;
+            suly = s;
+            ehese = e;
+        }
+
+        public Macska(string n, double s)
+        {
+            nev = n;
+            suly = s;
+            ehese = true;
+        }
+
+        public bool etetes(double mennyiseg)
+        {
+            if (ehese)
+            {
+                suly += mennyiseg;
+                ehese = false;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void futkos()
+        {
+            suly -= 0.1;
+            ehese = true;
+        }
+
+        public void kiiras()
+        {
+            if (ehese)
+            {
+                Console.WriteLine($"{nev} macska, {suly} kg, éhes a cica");
+            }
+            else
+            {
+                Console.WriteLine($"{nev} macska, {suly} kg, nem éhes a cica");
+            }
+        }
+    }
+}
---
+++ Gyakorlas/cicukaoop/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cicukaoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Macska szafika = new Macska("Szafi", 3.5, true);
+            Macska picurka = new Macska("Picur", 3.8);
+            szafika.kiiras();
+            picurka.kiiras();
+
+            Console.Write("Mennyit egyenek a ciciák:  ");
+            double kajamennyiseg = Convert.ToDouble(Console.ReadLine());
+            if (szafika.etetes(kajamennyiseg))
+            {
+                Console.WriteLine($"{szafika.Nev} evett.");
+            }
+            else
+            {
+                Console.WriteLine($"{szafika.Nev} nem evett.");
+            }
+
+            if (szafika.etetes(kajamennyiseg))
+            {
+                Console.WriteLine($"{picurka.Nev} evett.");
+            }
+            else
+            {
+                Console.WriteLine($"{picurka.Nev} nem evett.");
+            }
+
+            szafika.kiiras();
+            picurka.kiiras();
+
+            szafika.futkos();
+            picurka.futkos();
+
+            szafika.kiiras();
+            picurka.kiiras();
+
+
+            Console.ReadKey();
+        }
+    }
+}
