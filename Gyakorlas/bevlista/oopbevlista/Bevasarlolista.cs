aa5b21612dbe09404f50bcd84ebdf0c293b67e39
Gábor <baligabor@gmail.com>
2025-06-16 11:45:59 +0200

    First

---
+++ Gyakorlas/bevlista/oopbevlista/Bevasarlolista.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopbevlista
+{
+    internal class Bevasarlolista
+    {
+        string nev;
+        double egysegar;
+        int mennyiseg;
+
+        public string Nev { get => nev; set => nev = value; }
+        public double Egysegar { get => egysegar; set => egysegar = value; }
+        public int Mennyiseg { get => mennyiseg; set => mennyiseg = value; }
+
+
+        public Bevasarlolista(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            nev = darabok[0];
+            egysegar = Convert.ToDouble(darabok[1]);
+            mennyiseg = Convert.ToInt32(darabok[2]);
+        }
+
+        public double fizetendoSzamitas()
+        {
+            return egysegar * mennyiseg;
+        }
+    }
+}
---
+++ Gyakorlas/bevlista/oopbevlista/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopbevlista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bevasarlolista> ltermekek = new List<Bevasarlolista>();
+            FileStream fs = new FileStream("bevasarlolista.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            while (!sr.EndOfStream)
+            {
+                Bevasarlolista b = new Bevasarlolista(sr.ReadLine());
+                ltermekek.Add(b);
+            }
+
+            sr.Close();
+            fs.Close();
+
+            Console.WriteLine($"{ltermekek.Count} féle termék szerepel a bevásárlólistán.");
+
+            double vegosszeg = 0;
+            int mennyisegOsszeg = 0;
+            for (int i = 0; i < ltermekek.Count; i++)
+            {
+                vegosszeg += ltermekek[i].fizetendoSzamitas();
+                mennyisegOsszeg += ltermekek[i].Mennyiseg;
+            }
+            Console.WriteLine($"Végösszeg: {vegosszeg} Forint");
+            Console.WriteLine($"Összesen {mennyisegOsszeg} db termék");
+
+            int max = 0;
+            for (int i = 1; i < ltermekek.Count; i++)
+            {
+                if (ltermekek[i].Egysegar > ltermekek[max].Egysegar)
+                {
+                    max = i;
+                }
+            }
+            Console.WriteLine($"A legdrágább termék: {ltermekek[max].Nev}, ára: {ltermekek[max].Egysegar} Ft");
+
+            fs = new FileStream("fizetendo.txt",FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            for ( int i = 0; i < ltermekek.Count; i++)
+            {
+                sw.WriteLine($"{ltermekek[i].Nev};{ltermekek[i].fizetendoSzamitas()}");
+            }
+            sw.Write($"Végösszeg: {vegosszeg} Ft");
+            sw.Close();
+            fs.Close();
+
+            List<Bevasarlolista> lolcsok = new List<Bevasarlolista>();
+            for (int i = 0; i < ltermekek.Count; i++)
+            {
+                if (ltermekek[i].Egysegar < 500)
+                {
+                    lolcsok.Add(ltermekek[i]);
+                }
+            }
+
+            Console.WriteLine($"{lolcsok.Count} db 500 forintnál olcsóbb termék van.");
+
+            int j = 0;
+            while (j < ltermekek.Count && ltermekek[j].Mennyiseg <= 10)
+            {
+                j++;
+            }
+            if (j < ltermekek.Count)
+            {
+                Console.WriteLine(ltermekek[j].Nev);
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen termék!");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
