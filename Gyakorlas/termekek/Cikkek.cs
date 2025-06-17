e15dce8410b75d8cc496078b7c2d4fc47e0f79a4
Gábor <baligabor@gmail.com>
2025-06-17 11:24:16 +0200

    First

---
+++ Gyakorlas/termekek/Cikkek.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace termekek
+{
+    internal class Cikkek
+    {
+        private string cikkszam;
+        private string megnevezes;
+        private int beszerzesiar;
+        private int afakulcs;
+        private bool akcios;
+        private int kedvezmeny;
+
+        public string Cikkszam { get => cikkszam; set => cikkszam = value; }
+        public string Megnevezes { get => megnevezes; set => megnevezes = value; }
+        public int Beszerzesiar { get => beszerzesiar; set => beszerzesiar = value; }
+        public int Afakulcs { get => afakulcs; set => afakulcs = value; }
+        public bool Akcios { get => akcios; set => akcios = value; }
+        public int Kedvezmeny { get => kedvezmeny; set => kedvezmeny = value; }
+
+
+        public Cikkek(string c, string m, int b, int a, bool ak, int k)
+        {
+            cikkszam = c;
+            megnevezes = m;
+            beszerzesiar=b;
+            afakulcs=a;
+            akcios = ak;
+            kedvezmeny = k;
+        }
+
+
+        public double NettoSzamitas()
+        {
+            double netto_ar = beszerzesiar * 1.35;
+            return Math.Round(netto_ar, 2);
+        }
+
+
+        public int BruttoSzamitas()
+        {
+            double brutto_ar = NettoSzamitas() * (100 + afakulcs) / 100;
+            return Convert.ToInt32(brutto_ar);
+        }
+
+
+        public string AkciosUzenetIras()
+        {
+            if (akcios)
+            {
+                return $"A {cikkszam} {megnevezes} jelenleg akciós.";
+            }
+            else
+            {
+                return $"A {cikkszam} {megnevezes} jelenleg nem akciós.";
+            }
+        }
+
+        public int KedvarSzamitas()
+        {
+            double kedvezar = BruttoSzamitas() * (100 - kedvezmeny) / 100;
+            return Convert.ToInt32(kedvezar);
+        }
+    }
+}
---
+++ Gyakorlas/termekek/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace termekek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cikkszam : ");
+            string cikk = Console.ReadLine();
+            Console.Write("Megnevezés: ");
+            string nev = Console.ReadLine();
+            Console.Write("Beszerzési ár: ");
+            int bear = Convert.ToInt32(Console.ReadLine());
+            Console.Write("ÁFA kulcs: ");
+            int afa = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Akcios? ");
+            bool akc;
+            int kedv;
+            if (Console.ReadLine() == "Igen")
+            {
+                akc = true;
+                Console.Write("Kedvezmény mértéke: ");
+                kedv = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                akc = false;
+                kedv = 0;
+            }
+           
+            Cikkek uj_termek = new Cikkek(cikk, nev, bear, afa, akc, kedv);
+
+            Console.WriteLine($"Nettó ár: {uj_termek.NettoSzamitas()}");
+            Console.WriteLine($"Bruttó ár: {uj_termek.BruttoSzamitas()}");
+            string uzenet = uj_termek.AkciosUzenetIras();
+            Console.WriteLine(uzenet);
+            if (akc)
+            {
+                Console.WriteLine($"Kedvezményes ár: {uj_termek.KedvarSzamitas()}");
+            }
+
+        }
+    }
+}
