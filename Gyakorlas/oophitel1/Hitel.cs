43925e5b5b1a16e6ab789ae2e3a1c48821a1f0f3
Gábor <baligabor@gmail.com>
2025-06-18 14:08:22 +0200

    First

---
+++ Gyakorlas/oophitel1/Hitel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oophitel
+{
+    internal class Hitel
+    {
+        public double kamat;
+        public double felvettosszeg;
+        public int evekszama;
+        public const double kezelesikoltseg = 0.05;
+
+        public Hitel(double k, double f, int e)
+        {
+            kamat = k;
+            felvettosszeg = f;  
+            evekszama = e;
+        }
+
+        public double visszafizetendo()
+        {
+            return Math.Round(felvettosszeg + felvettosszeg * kamat / 100 + felvettosszeg * kezelesikoltseg / 100);
+        }
+
+        public double havitorleszto()
+        {
+            return Math.Round(visszafizetendo() / (evekszama * 12));
+        }
+    }
+}
---
+++ Gyakorlas/oophitel1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oophitel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg a felvett összeget:  ");
+            double osszeg = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a kamatot:  ");
+            double kamat = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Adja meg a törlesztési évek számát:  ");
+            int evek = Convert.ToInt32(Console.ReadLine());
+
+            Hitel h = new Hitel(kamat, osszeg, evek);
+
+            Console.WriteLine($"A visszafizetendő összeg: {h.visszafizetendo()} Ft");
+            Console.WriteLine($"Havi törlesztőrészlet: {h.havitorleszto()} Ft");
+
+
+            Console.ReadKey();
+        }
+    }
+}
