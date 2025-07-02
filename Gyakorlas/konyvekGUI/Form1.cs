2d0904b1f1117fa308e0d44462739fe7ea555335
Gábor <baligabor@gmail.com>
2025-07-02 17:31:46 +0200

    First

---
+++ Gyakorlas/konyvekGUI/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace konyvekGUI
+{
+    public partial class Form1 : Form
+    {
+        List<Konyvek> konyveklista = new List<Konyvek>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("konyvekGUI_adatok.txt", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            double atlag = 0;
+
+            while (!sr.EndOfStream)
+            {
+                Konyvek a = new Konyvek (sr.ReadLine());
+                konyveklista.Add (a);
+
+                richTextBox1.Text += $"{a.Isbn}, {a.KonyvCime} ({a.KiadasEve}): {a.KonyvTipusa}\n";
+
+                atlag += a.KonyvHossza;
+            }
+
+            fs.Close();
+            sr.Close();
+            
+            label3.Text = $"A könyvek átlag hossza: {Math.Round(atlag / konyveklista.Count, 0)}";
+            label3.Visible = true;
+
+            int min = 0;
+
+            for (int i = 1;  i < konyveklista.Count; i++)
+            {
+                if (konyveklista[i].KiadasEve < konyveklista[min].KiadasEve)
+                {
+                    min = i;
+                }
+            }
+
+            textBox1.Text = konyveklista[min].KonyvCime;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("hangoskonyvek.csv", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for (int i = 0; i < konyveklista.Count; ++i)
+            {
+                if (konyveklista[i].KonyvTipusa == "hangoskönyv")
+                {
+                    sw.WriteLine($"{konyveklista[i].Isbn};{konyveklista[i].KonyvCime};{konyveklista[i].KonyvKiadoja};{konyveklista[i].KiadasEve}");
+                }
+            }
+
+            sw.Close();
+            fs.Close();
+
+            MessageBox.Show("hangoskonyvek.csv létrehozva!", "Üzenet", MessageBoxButtons.OK, MessageBoxIcon.Information );
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int darab = 0;
+
+            for (int i = 0; i < konyveklista.Count; i++)
+            {
+                if (konyveklista[i].KonyvTipusa == "e-könyv")
+                {
+                    darab++;
+                }
+            }
+
+            MessageBox.Show($"{darab} db e-könyv van", "e-könyvek száma", MessageBoxButtons.OK, MessageBoxIcon.Information );
+        }
+    }
+}
---
+++ Gyakorlas/konyvekGUI/Konyvek.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konyvekGUI
+{
+    internal class Konyvek
+    {
+        string isbn;
+        string konyvCime;
+        int kiadasEve;
+        string konyvTipusa;
+        string konyvKiadoja;
+        int konyvHossza;
+
+        public string Isbn { get => isbn; set => isbn = value; }
+        public string KonyvCime { get => konyvCime; set => konyvCime = value; }
+        public int KiadasEve { get => kiadasEve; set => kiadasEve = value; }
+        public string KonyvTipusa { get => konyvTipusa; set => konyvTipusa = value; }
+        public string KonyvKiadoja { get => konyvKiadoja; set => konyvKiadoja = value; }
+        public int KonyvHossza { get => konyvHossza; set => konyvHossza = value; }
+
+
+        public Konyvek(string sor)
+        {
+            var adat = sor.Split(';');
+            isbn = adat[0];
+            konyvCime = adat[1];
+            kiadasEve = Convert.ToInt32(adat[2]);
+
+            switch (adat[3]){
+                case "e": konyvTipusa = "e-könyv"; break;
+                case "h": konyvTipusa = "hangoskönyv"; break;
+                case "n": konyvTipusa = "könyv"; break;
+            }
+    
+            konyvKiadoja = adat[4];
+            konyvHossza = Convert.ToInt32(adat[5]);
+        }
+    }
+}
